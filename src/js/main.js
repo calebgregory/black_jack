f6c04befbaff1aea7fa5c9cd4dcb7a3b4a3804eb
calebgregory <calebgregory@gmail.com>
2015-05-15 01:51:01 -0500

    Adding minor changes

--- src/js/main.js
+++ src/js/main.js
@@ -16,16 +16,12 @@ getJSON('http://deckofcardsapi.com/api/shuffle/?deck_count=1', function (d) {
 function getJSON(url, cb) {
   var JSONP_PROXY = 'https://jsonp.afeld.me/?url=';
   // THIS WILL ADD THE CROSS ORIGIN HEADERS
-
   var request = new XMLHttpRequest();
-
   request.open('GET', JSONP_PROXY + url);
-
   request.onload = function() {
     if (request.status >= 200 && request.status < 400) {
       cb(JSON.parse(request.responseText));
     }
   };
-
   request.send();
 }
